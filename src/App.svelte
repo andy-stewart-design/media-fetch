0624e03223eca23d887b95eb107a115ce7bf2b98
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2023-04-15 15:31:20 -0700

    Initial setup for Pexels and Unsplash image search

--- src/App.svelte
+++ src/App.svelte
@@ -1,67 +1,95 @@
 <script lang="ts">
-  import { fade } from "svelte/transition"
-  import "./app.css"
+  import search from "./components/svg/search.svg";
+  import close from "./components/svg/close.svg";
+  import { fade } from "svelte/transition";
+  import "./app.css";
 
   interface ImageArray {
-    src: string
-    thumb: string
-    width: string
-    height: string
-    photographer: string
+    src: string;
+    thumb: string;
+    width: string;
+    height: string;
+    photographer: string;
+    service: string;
   }
 
-  let query: string
-  let images: ImageArray[]
+  let query: string;
+  let images: ImageArray[];
 
   onmessage = (event) => {
     if (event.data.pluginMessage.images) {
-      images = event.data.pluginMessage.images
+      images = event.data.pluginMessage.images;
     }
-  }
+  };
 
   const postSearch = () => {
     if (query && query.replaceAll(" ", "") !== "") {
-      parent.postMessage({ pluginMessage: { type: "SEARCH", query } }, "*")
-    } else console.log("need a search query")
-  }
+      parent.postMessage({ pluginMessage: { type: "SEARCH", query } }, "*");
+    } else console.log("need a search query");
+  };
+  const clearSearch = () => (query = "");
 
-  const postCreate = (src: string) => parent.postMessage({ pluginMessage: { type: "CREATE", src } }, "*")
+  const postCreate = (src: string) => parent.postMessage({ pluginMessage: { type: "CREATE", src } }, "*");
 
-  const handleClick = (e: KeyboardEvent) => e.key === "Enter" && postSearch()
+  const handleClick = (e: KeyboardEvent) => e.key === "Enter" && postSearch();
 </script>
 
-<div class="flex flex-col min-h-[100%]">
-  <div class="sticky top-0 flex gap-2 bg-figma-gray-900 border-b border-white/10 p-4 z-50">
-    <input
-      class="grow bg-transparent border border-white/10 pt-1.5 px-2 pb-2 focus:outline-none focus-visible:border-blue-600"
-      type="text"
-      bind:value={query}
-      placeholder="Search for images"
-      on:keydown={handleClick}
-    />
-    <button class="bg-blue-600 pt-1.5 px-4 pb-2 focus:outline-none focus-visible:outline-blue-600" on:click={postSearch}
-      >Search</button
-    >
+<div class="flex min-h-[100%] flex-col">
+  <div class="sticky top-0 z-50 border-b border-white/10 bg-figma-gray-900 p-4">
+    <div class="flex h-12 border border-white/10">
+      <button
+        class="flex w-11 items-center justify-center focus:outline-none focus-visible:outline-blue-600 disabled:opacity-50"
+        aria-label="Search"
+        disabled={!(query && query.replaceAll(" ", "") !== "")}
+        on:click={postSearch}
+      >
+        {@html search}
+      </button>
+      <input
+        class="grow bg-transparent pt-1.5 pb-2 placeholder:text-white/50 focus:outline-none focus-visible:border-blue-600"
+        type="text"
+        bind:value={query}
+        placeholder="Search for images"
+        on:keydown={handleClick}
+      />
+      <button
+        class="flex w-11 items-center justify-center focus:outline-none focus-visible:outline-blue-600 disabled:opacity-0"
+        aria-label="Clear search"
+        disabled={!(query && query.replaceAll(" ", "") !== "")}
+        on:click={clearSearch}
+      >
+        {@html close}
+      </button>
+    </div>
   </div>
   {#if images}
     {#key images}
       <div class="grid grid-cols-2 gap-4 p-4" in:fade>
         {#each images as image}
           <div class="flex flex-col gap-1">
-            <button on:click={() => postCreate(image.src)} class="grow bg-white/5 border border-white/10">
+            <button
+              class="group relative grow border border-white/10 bg-white/5"
+              on:click={() => postCreate(image.src)}
+            >
               <img src={image.thumb} alt={image.photographer} width={image.width} height={image.height} />
+              <div
+                class="absolute top-0 left-0 flex h-full w-full items-center justify-center bg-black/40 font-semibold opacity-0 group-hover:opacity-100"
+              >
+                Add Image
+              </div>
             </button>
             <p class="text-[11px]">
               <span class="opacity-60">by</span>
-              {image.photographer} <span class="opacity-60">from</span> Pexels
+              <span class="font-semibold">{image.photographer}</span> <span class="opacity-60">from</span>
+              <span class="font-semibold capitalize">{image.service}</span>
             </p>
           </div>
         {/each}
       </div>
     {/key}
   {:else}
-    <div class="grow flex justify-center items-center">
-      <p class="opacity-50 mb-4">Enter a search term</p>
+    <div class="flex grow items-center justify-center">
+      <p class="mb-4 opacity-50">Enter a search term</p>
     </div>
   {/if}
 </div>
--- src/code.ts
+++ src/code.ts
@@ -1,74 +1,81 @@
+// TODO: Integrate Unsplash + Pixabay
 // TODO: Handle error/rejection
 // TODO: Enable video search
-// TODO: Integrate Unsplash + Pixabay
 
-type PhotoSource = "UNSPLASH" | "PEXELS"
+type PhotoSource = "UNSPLASH" | "PEXELS" | "PIXABAY";
 
 interface SearchMessage {
-  type: "SEARCH"
-  query: string
+  type: "SEARCH";
+  query: string;
 }
 
 interface CreateMessage {
-  type: "CREATE"
-  src: string
+  type: "CREATE";
+  src: string;
 }
 
-figma.showUI(__html__, { themeColors: true, width: 480, height: 560 })
+figma.showUI(__html__, { themeColors: true, width: 480, height: 560 });
 
-const pexelsBaseURL = "https://api.pexels.com/v1/search"
-const pexelsAPIKey = "6g13kUgrPvTdfvJ0TfNQS2QXzVBLFqO0tu5gMQTCaIOQCGYWISSckCPs"
-const unsplashBaseURL = "https://api.unsplash.com/photos/random?client_id="
-const unsplashAPIKey = "tAWVEwm8Gkhpp9r8wNTDyS_sgLptx6uEuqTm6_Hx6os"
+const pexelsBaseURL = "https://api.pexels.com/v1/search";
+const pexelsAPIKey = "6g13kUgrPvTdfvJ0TfNQS2QXzVBLFqO0tu5gMQTCaIOQCGYWISSckCPs";
+const unsplashBaseURL = "https://api.unsplash.com/search/photos";
+const unsplashAPIKey = "tAWVEwm8Gkhpp9r8wNTDyS_sgLptx6uEuqTm6_Hx6os";
 
 figma.ui.onmessage = async (msg: SearchMessage | CreateMessage) => {
   if (msg.type === "SEARCH") {
-    // const res = await fetch(`https://api.pexels.com/v1/search?query=${msg.query}&per_page=50`, {
-    //   method: "GET",
-    //   headers: {
-    //     Accept: "application/json",
-    //     Authorization: pexelsAPIKey,
-    //   },
-    // })
-    // const json = await res.json()
-    const url1 = createFetchURL("PEXELS", msg.query, 25)
-    const url2 = createFetchURL("PEXELS", "seattle", 25)
+    const fetchParams1 = createFetchParams("UNSPLASH", msg.query, 25);
+    const fetchParams2 = createFetchParams("PEXELS", msg.query, 25);
 
-    const foo = await Promise.allSettled([getPhotos(url1), getPhotos(url2)])
-    const bar = foo.map((set) => {
-      if (set.status === "fulfilled") return set.value
-    })
-    const images = bar.flat().map((image) => {
-      return {
-        photographer: image.photographer,
-        thumb: image.src.medium,
-        src: image.src.large2x,
-        width: image.width,
-        height: image.height,
+    const foo = await Promise.allSettled([fetchPhotos(fetchParams1), fetchPhotos(fetchParams2)]);
+    const bar = foo.map((result) => {
+      if (result.status === "fulfilled") {
+        const set = result.value;
+        if (set.service === "PEXELS") {
+          return set.photos.map((image) => {
+            const orientation = image.width > image.height ? "landscape" : "portrait";
+            return {
+              service: set.service.toLocaleLowerCase(),
+              photographer: image.photographer,
+              thumb: image.src.medium,
+              src: image.src.large2x,
+              width: image.width,
+              height: image.height,
+              orientation,
+            };
+          });
+        } else if (set.service === "UNSPLASH") {
+          return set.results.map((image) => {
+            const orientation = image.width > image.height ? "landscape" : "portrait";
+            const src =
+              orientation === "landscape"
+                ? image.urls.regular.replace("w=1080", "w=1920")
+                : image.urls.regular.replace("w=1080", "w=1280");
+            return {
+              service: set.service.toLocaleLowerCase(),
+              photographer: image.user.name,
+              thumb: image.urls.small,
+              src,
+              width: image.width,
+              height: image.height,
+              orientation,
+            };
+          });
+        }
       }
-    })
+    });
+    console.log(bar);
 
-    // const rawImageData = await getPhotos(msg.query)
-    // const images = rawImageData.map((image) => {
-    //   return {
-    //     photographer: image.photographer,
-    //     thumb: image.src.medium,
-    //     src: image.src.large2x,
-    //     width: image.width,
-    //     height: image.height,
-    //   }
-    // })
-    figma.ui.postMessage({ images: shuffle(images) })
+    figma.ui.postMessage({ images: shuffle(bar.flat()) });
   } else if (msg.type === "CREATE") {
-    const imgData = await figma.createImageAsync(msg.src)
-    const { width, height } = await imgData.getSizeAsync()
+    const imgData = await figma.createImageAsync(msg.src);
+    const { width, height } = await imgData.getSizeAsync();
 
-    const { x, y } = figma.viewport.center
+    const { x, y } = figma.viewport.center;
 
-    const rect = figma.createRectangle()
-    rect.resize(width, height)
-    rect.x = x - width / 2
-    rect.y = y - height / 2
+    const rect = figma.createRectangle();
+    rect.resize(width, height);
+    rect.x = x - width / 2;
+    rect.y = y - height / 2;
 
     rect.fills = [
       {
@@ -76,36 +83,61 @@ figma.ui.onmessage = async (msg: SearchMessage | CreateMessage) => {
         imageHash: imgData.hash,
         scaleMode: "FILL",
       },
-    ]
+    ];
 
-    figma.closePlugin()
+    // figma.closePlugin();
   }
-}
+};
 
-const getPhotos = async (url: string) => {
-  const res = await fetch(url, {
-    method: "GET",
-    headers: {
-      Accept: "application/json",
-      Authorization: pexelsAPIKey,
-    },
-  })
-  const json = await res.json()
-  return json.photos
-}
-
-const createFetchURL = (source: PhotoSource, query: string, amount: number) => {
-  if (source === "PEXELS") {
-    return `${pexelsBaseURL}?query=${query}&per_page=${amount}`
+const createFetchParams = (service: PhotoSource, query: string, amount: number) => {
+  let url: string;
+  switch (service) {
+    case "PEXELS":
+      url = `${pexelsBaseURL}?query=${query}&per_page=${amount}`;
+      break;
+    case "UNSPLASH":
+      url = `${unsplashBaseURL}?client_id=${unsplashAPIKey}&query=${query}&per_page=${amount}`;
+      break;
+    case "PIXABAY":
+      console.log("Still need to set up pixabay");
+      break;
+    default:
+      console.error("Invalid service specified");
   }
-}
+  return { service, url };
+};
+
+const fetchPhotos = async ({ service, url }: { service: PhotoSource; url: string }) => {
+  const target = { service };
+  switch (service) {
+    case "PEXELS":
+      const pexelsResults = await fetch(url, {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          Authorization: pexelsAPIKey,
+        },
+      });
+      const pexelsJSON = await pexelsResults.json();
+      return Object.assign(target, pexelsJSON);
+    case "UNSPLASH":
+      const unsplashResults = await fetch(url);
+      const unsplashJSON = await unsplashResults.json();
+      return Object.assign(target, unsplashJSON);
+    case "PIXABAY":
+      console.log("Still need to set up pixabay");
+      break;
+    default:
+      console.error("Invalid service specified");
+  }
+};
 
 function shuffle<T>(arr: T[]): T[] {
   for (let i = arr.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1))
-    const temp = arr[i]
-    arr[i] = arr[j]
-    arr[j] = temp
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
   }
-  return arr
+  return arr;
 }
