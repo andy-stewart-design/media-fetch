917068ca5918df161df6c734dff5a3632a2fbb9d
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2023-04-19 07:43:10 -0700

    refining search functionality

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
+  // TODO: filters (amount, orientation, color)
+
   import { fade } from "svelte/transition";
   import ImageCard from "./components/ImageCard.svelte";
   import search from "./components/svg/search.svg";
   import close from "./components/svg/close.svg";
   import loading from "./components/svg/loading.svg";
   import "./app.css";
-  import VideoCard from "./components/VideoCard.svelte";
-
-  type MediaType = "IMAGE" | "VIDEO";
 
   interface MediaEntry {
     src: string;
@@ -18,29 +17,27 @@
     service: string;
   }
 
-  interface VideoEntry extends MediaEntry {
-    videoThumb: string;
-  }
-
   let isWorking = false;
-  let mediaType: MediaType = "IMAGE";
   let query: string;
   let imageArray: MediaEntry[];
-  let videoArray: VideoEntry[];
 
   onmessage = (event) => {
     if (event.data.pluginMessage.messageType === "STATUS") {
+      console.log("message received");
+
       isWorking = event.data.pluginMessage.workInProgress;
     }
     if (event.data.pluginMessage.media) {
-      if (event.data.pluginMessage.mediaType === "IMAGE") imageArray = event.data.pluginMessage.media;
-      else if (event.data.pluginMessage.mediaType === "VIDEO") videoArray = event.data.pluginMessage.media;
+      imageArray = event.data.pluginMessage.media;
     }
   };
 
   const postSearch = () => {
     if (query && query.replaceAll(" ", "") !== "") {
-      parent.postMessage({ pluginMessage: { messageType: "SEARCH", mediaType, query } }, "*");
+      const searchQuery = query.replaceAll(" ", "+");
+      console.log(query, searchQuery);
+
+      parent.postMessage({ pluginMessage: { messageType: "SEARCH", query: searchQuery } }, "*");
     } else console.log("need a search query");
   };
 
@@ -49,27 +46,13 @@
   const postCreate = (src: string, width: number, height: number) => {
     console.log("posting create", { src, width, height });
 
-    parent.postMessage({ pluginMessage: { messageType: "CREATE", mediaType, src, width, height } }, "*");
+    parent.postMessage({ pluginMessage: { messageType: "CREATE", src, width, height } }, "*");
   };
 
   const handleClick = (e: KeyboardEvent) => e.key === "Enter" && postSearch();
 </script>
 
 <div class="relative flex min-h-[100%] flex-col">
-  <div class="flex">
-    <button
-      class="grow border-t-2 border-transparent bg-figma-gray-900/75 p-3 aria-checked:border-current aria-checked:bg-figma-gray-800"
-      on:click={() => (mediaType = "IMAGE")}
-      role="radio"
-      aria-checked={mediaType === "IMAGE"}>Images</button
-    >
-    <button
-      class="grow border-t-2 border-transparent bg-figma-gray-900/75 p-3 aria-checked:border-current aria-checked:bg-figma-gray-800"
-      on:click={() => (mediaType = "VIDEO")}
-      role="radio"
-      aria-checked={mediaType === "VIDEO"}>Video</button
-    >
-  </div>
   <div class="sticky top-0 z-50 border-b border-white/10 bg-figma-gray-800 py-3 px-4">
     <div class="flex h-12">
       <button
@@ -84,7 +67,7 @@
         class="grow bg-transparent pt-1.5 pb-2 placeholder:text-white/50 focus:outline-none focus-visible:border-blue-600"
         type="text"
         bind:value={query}
-        placeholder={`Search for ${mediaType.toLocaleLowerCase()}s`}
+        placeholder={`Search for images`}
         on:keydown={handleClick}
         spellcheck="false"
       />
@@ -98,16 +81,10 @@
       </button>
     </div>
   </div>
-  {#if mediaType === "IMAGE" && imageArray}
+  {#if imageArray}
     <div class="grid grid-cols-2 gap-4 bg-figma-gray-900/60 p-4" in:fade>
       {#each imageArray as image}
         <ImageCard {image} on:click={() => postCreate(image.src, image.width, image.height)} />
-      {/each}
-    </div>
-  {:else if mediaType === "VIDEO" && videoArray}
-    <div class="grid grid-cols-2 gap-4 bg-figma-gray-900/60 p-4" in:fade>
-      {#each videoArray as video}
-        <VideoCard {video} on:click={() => postCreate(video.src, video.width, video.height)} />
       {/each}
     </div>
   {:else}
@@ -116,7 +93,7 @@
     </div>
   {/if}
   {#if isWorking}
-    <div class="absolute top-0 left-0 z-50 flex h-full w-full items-center justify-center bg-black/70">
+    <div class="fixed top-0 left-0 z-50 flex h-screen w-screen items-center justify-center bg-black/70">
       {@html loading}
     </div>
   {/if}
--- src/code.ts
+++ src/code.ts
@@ -1,6 +1,4 @@
-// TODO: Integrate Unsplash + Pixabay
 // TODO: Handle error/rejection
-// TODO: Enable video search
 
 type PhotoSource = "UNSPLASH" | "PEXELS" | "PIXABAY";
 
@@ -8,192 +6,130 @@ type MediaType = "IMAGE" | "VIDEO";
 
 interface SearchMessage {
   messageType: "SEARCH";
-  mediaType: MediaType;
   query: string;
 }
 
 interface CreateMessage {
   messageType: "CREATE";
-  mediaType: MediaType;
   src: string;
   width: number;
   height: number;
 }
 
-figma.showUI(__html__, { themeColors: true, width: 480, height: 560 });
+figma.showUI(__html__, { themeColors: true, width: 560, height: 560 });
 
 const pexelsAPIKey = "6g13kUgrPvTdfvJ0TfNQS2QXzVBLFqO0tu5gMQTCaIOQCGYWISSckCPs";
-const pexelsBaseImageURL = "https://api.pexels.com/v1/search";
-const pexelsBaseVideoURL = "https://api.pexels.com/videos/search";
+const pexelsBaseURL = "https://api.pexels.com/v1/search";
 const unsplashAPIKey = "tAWVEwm8Gkhpp9r8wNTDyS_sgLptx6uEuqTm6_Hx6os";
 const unsplashBaseURL = "https://api.unsplash.com/search/photos";
 const pixabayAPIKey = "35568846-b12b8564471b5e493ec192e02";
-const pixabayBaseImageURL = "https://pixabay.com/api/";
-const pixabayBaseVideoURL = "https://pixabay.com/api/videos/";
+const pixabayBaseURL = "https://pixabay.com/api/";
 
 figma.ui.onmessage = async (msg: SearchMessage | CreateMessage) => {
   if (msg.messageType === "SEARCH") {
-    if (msg.mediaType === "VIDEO") {
-      console.log("Searching for video");
+    const fetchParams1 = createFetchParams("UNSPLASH", msg.query, 12);
+    const fetchParams2 = createFetchParams("PEXELS", msg.query, 12);
+    const fetchParams3 = createFetchParams("PIXABAY", msg.query, 12);
 
-      const fetchParams = createFetchParams("PIXABAY", msg.mediaType, msg.query, 10);
+    const results = await Promise.allSettled([
+      fetchMedia(fetchParams1),
+      fetchMedia(fetchParams2),
+      fetchMedia(fetchParams3),
+    ]);
 
-      const videoDataRaw = await fetchMedia(fetchParams);
+    const fulfilledResults = results.filter(isFulfilled).map((p) => p.value);
+    const rejectedResults = results.filter(isRejected);
 
-      console.log(videoDataRaw);
-
-      const videoData = videoDataRaw.hits.map((video, index) => {
-        const orientation = video.videos.large.width > video.videos.large.height ? "landscape" : "portrait";
-        // const getVideoSrc = (files: any[], size: string) => {
-        //   let targetWidth = undefined;
-        //   if (size === "MAX") {
-        //     targetWidth = Math.max.apply(
-        //       Math,
-        //       video.video_files.map((file) => (file.width < 2000 ? file.width : 0))
-        //     );
-        //   } else if (size === "MIN") {
-        //     targetWidth = Math.min.apply(
-        //       Math,
-        //       video.video_files.map((file) => file.width)
-        //     );
-        //   }
-
-        //   return files.filter((file) => file.width === targetWidth)[0];
-        // };
-
-        // const videoSrc = getVideoSrc(video.video_files, "MAX");
-        // const videoThumb = getVideoSrc(video.video_files, "MIN");
-
-        return {
-          service: videoDataRaw.service.toLocaleLowerCase(),
-          id: video.id,
-          creator: video.user,
-          // thumb: video.image,
-          videoThumb: video.videos.small.url,
-          src: video.videos.large.url,
-          width: video.videos.large.width || undefined,
-          height: video.videos.large.height || undefined,
-          orientation,
-        };
-      });
-
-      console.log(videoData);
-
-      figma.ui.postMessage({ mediaType: msg.mediaType, media: shuffle(videoData.flat()) });
-    } else if (msg.mediaType === "IMAGE") {
-      const fetchParams1 = createFetchParams("UNSPLASH", msg.mediaType, msg.query, 25);
-      const fetchParams2 = createFetchParams("PEXELS", msg.mediaType, msg.query, 25);
-
-      const imageDataRaw = await Promise.allSettled([fetchMedia(fetchParams1), fetchMedia(fetchParams2)]);
-      const imageData = imageDataRaw.map((result) => {
-        if (result.status === "fulfilled") {
-          const set = result.value;
-          if (set.service === "PEXELS") {
-            return set.photos.map((image) => {
-              const orientation = image.width > image.height ? "landscape" : "portrait";
-              return {
-                service: set.service.toLocaleLowerCase(),
-                creator: image.photographer,
-                thumb: image.src.medium,
-                src: image.src.large2x,
-                width: image.width,
-                height: image.height,
-                orientation,
-              };
-            });
-          } else if (set.service === "UNSPLASH") {
-            return set.results.map((image) => {
-              const orientation = image.width > image.height ? "landscape" : "portrait";
-              const src =
-                orientation === "landscape"
-                  ? image.urls.regular.replace("w=1080", "w=1920")
-                  : image.urls.regular.replace("w=1080", "w=1280");
-              return {
-                service: set.service.toLocaleLowerCase(),
-                photographer: image.user.name,
-                thumb: image.urls.small,
-                src,
-                width: image.width,
-                height: image.height,
-                orientation,
-              };
-            });
-          }
-        }
-      });
-      // console.log(imageData);
-
-      figma.ui.postMessage({ mediaType: msg.mediaType, media: shuffle(imageData.flat()) });
+    if (fulfilledResults.length === 0) {
+      console.error("No results found");
+      return;
     }
-  } else if (msg.messageType === "CREATE") {
-    if (msg.mediaType === "VIDEO") {
-      figma.ui.postMessage({ messageType: "status", workInProgress: true });
-      console.log("creating video", msg);
-      const response = await fetch(msg.src);
-      if (!response.ok) {
-        throw new Error("Network response was not OK");
+    if (rejectedResults.length > 0) console.log("Rejected results: ", rejectedResults);
+
+    const imageData = fulfilledResults.map((set) => {
+      if (set.service === "PEXELS") {
+        return set.photos.map((image) => {
+          const orientation = image.width > image.height ? "landscape" : "portrait";
+          return {
+            service: set.service.toLocaleLowerCase(),
+            creator: image.photographer,
+            thumb: image.src.medium,
+            src: image.src.large2x,
+            width: image.width,
+            height: image.height,
+            orientation,
+          };
+        });
+      } else if (set.service === "UNSPLASH") {
+        return set.results.map((image) => {
+          const orientation = image.width > image.height ? "landscape" : "portrait";
+          const src =
+            orientation === "landscape"
+              ? image.urls.regular.replace("w=1080", "w=1920")
+              : image.urls.regular.replace("w=1080", "w=1280");
+          return {
+            service: set.service.toLocaleLowerCase(),
+            creator: image.user.name,
+            thumb: image.urls.small,
+            src,
+            width: image.width,
+            height: image.height,
+            orientation,
+          };
+        });
+      } else if (set.service === "PIXABAY") {
+        return set.hits.map((image) => {
+          const orientation = image.width > image.height ? "landscape" : "portrait";
+          const src = image.fullHDURL ? image.fullHDURL : image.largeImageURL;
+          return {
+            service: set.service.toLocaleLowerCase(),
+            creator: image.user,
+            thumb: image.webformatURL,
+            src,
+            width: image.imageWidth,
+            height: image.imageHeight,
+            orientation,
+          };
+        });
       }
-      const videobuffer = await response.arrayBuffer();
-      const videoUint8Array = new Uint8Array(videobuffer);
+    });
 
-      const videoData = await figma.createVideoAsync(videoUint8Array);
+    console.log(fulfilledResults);
+    console.log(imageData);
 
-      const { x, y } = figma.viewport.center;
+    figma.ui.postMessage({ media: shuffle(imageData.flat()) });
+  } else if (msg.messageType === "CREATE") {
+    figma.ui.postMessage({ messageType: "STATUS", workInProgress: true });
+    const imgData = await figma.createImageAsync(msg.src);
 
-      const rect = figma.createRectangle();
-      rect.resize(msg.width, msg.height);
-      rect.x = x - msg.width / 2;
-      rect.y = y - msg.height / 2;
+    const { x, y } = figma.viewport.center;
 
-      rect.fills = [
-        {
-          type: "VIDEO",
-          videoHash: videoData.hash,
-          scaleMode: "FILL",
-        },
-      ];
-      figma.ui.postMessage({ messageType: "status", workInProgress: false });
-      // figma.closePlugin();
-    } else if (msg.mediaType === "IMAGE") {
-      const imgData = await figma.createImageAsync(msg.src);
+    const rect = figma.createRectangle();
+    rect.resize(msg.width, msg.height);
+    rect.x = x - msg.width / 2;
+    rect.y = y - msg.height / 2;
 
-      const { x, y } = figma.viewport.center;
-
-      const rect = figma.createRectangle();
-      rect.resize(msg.width, msg.height);
-      rect.x = x - msg.width / 2;
-      rect.y = y - msg.height / 2;
-
-      rect.fills = [
-        {
-          type: "IMAGE",
-          imageHash: imgData.hash,
-          scaleMode: "FILL",
-        },
-      ];
-
-      // figma.closePlugin();
-    }
+    rect.fills = [
+      {
+        type: "IMAGE",
+        imageHash: imgData.hash,
+        scaleMode: "FILL",
+      },
+    ];
+    figma.ui.postMessage({ messageType: "STATUS", workInProgress: false });
+    // figma.closePlugin();
   }
 };
 
-const createFetchParams = (service: PhotoSource, mediaType: MediaType, query: string, amount = 20) => {
+const createFetchParams = (service: PhotoSource, query: string, amount = 20) => {
   let url: string;
 
   if (service === "PEXELS") {
-    if (mediaType === "IMAGE") {
-      url = `${pexelsBaseImageURL}?query=${query}&per_page=${amount}`;
-    } else if (mediaType === "VIDEO") {
-      url = `${pexelsBaseVideoURL}?query=${query}&per_page=${amount}`;
-    }
+    url = `${pexelsBaseURL}?query=${query}&per_page=${amount}`;
   } else if (service === "UNSPLASH") {
     url = `${unsplashBaseURL}?client_id=${unsplashAPIKey}&query=${query}&per_page=${amount}`;
   } else if (service === "PIXABAY") {
-    if (mediaType === "IMAGE") {
-      url = `${pixabayBaseImageURL}?key=${pixabayAPIKey}&q=${query}&per_page=${amount}`;
-    } else if (mediaType === "VIDEO") {
-      url = `${pixabayBaseVideoURL}?key=${pixabayAPIKey}&q=${query}&per_page=${amount}`;
-    }
+    url = `${pixabayBaseURL}?key=${pixabayAPIKey}&q=${query}&per_page=${amount}`;
   } else {
     console.error("Invalid service specified");
   }
@@ -236,3 +172,6 @@ function shuffle<T>(arr: T[]): T[] {
   }
   return arr;
 }
+
+const isFulfilled = <T>(p: PromiseSettledResult<T>): p is PromiseFulfilledResult<T> => p.status === "fulfilled";
+const isRejected = <T>(p: PromiseSettledResult<T>): p is PromiseRejectedResult => p.status === "rejected";
